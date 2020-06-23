928cecda917e3f9002b445a3fc87dc586ea19db1
bitinbyte <bitinbyte@outlook.com>
2020-06-23 10:28:56 +0100

    Bug fixed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const timeContainer = document.querySelector(".container__watch");
+
 // Variables object
 const state = {
 	h: 0,
@@ -7,7 +9,7 @@ const state = {
 };
 
 // debug
-// window.state = state;
+window.state = state;
 
 const init = () => {
 
@@ -33,6 +35,12 @@ const time = () => {
 	const s = d.getSeconds().toString().padStart(2, "0");
 	const m = d.getMinutes().toString().padStart(2, "0");
 	const h = d.getHours().toString().padStart(2, "0");
+	// timeContainer.textContent = h + ":" + m + ":" + s;
+	// timeContainer.textContent = `${h}:${m}:${s}`;
+
+	// console.log(document.querySelector(".animate-down"));
+
+	
 
 	// Create the new markup
 	const markup = `
@@ -45,6 +53,9 @@ const time = () => {
 	
 	// Render the markup
 	document.querySelector(`.${e}`).insertAdjacentHTML("afterbegin", markup);
+
+	// console.log(document.querySelector(".container__watch--second-1").classList.contains("animate-up"));
+
 	
 	// Save the actual time to compare and launch the animation on the clock later
 	state.h = h;
@@ -71,6 +82,10 @@ const binary = (date) => {
 	const secondsClass = "container__binary-watch--second";
 
 	// Block objects to perform the convertion
+	// let convertedHours = [];
+	// let convertedMinutes = [];
+	// let convertedSeconds = [];
+
 	const convertHours = date.getHours();
 	const convertMinutes = date.getMinutes();
 	const convertSeconds = date.getSeconds();
@@ -80,10 +95,90 @@ const binary = (date) => {
 	const convertedMinutes = convertTimeToBinary(convertMinutes);
 	const convertedSeconds = convertTimeToBinary(convertSeconds);
 
-	// Performs the markup mapping all of the elemtns of the arrays
+	// do {
+
+	// 	if(convertHours > 0){
+	// 		convertedHours.push(Math.floor(convertHours % 2));
+
+	// 		convertHours = convertHours / 2;
+	// 	} else if (Math.floor(convertHours) === 1 || Math.floor(convertHours) === 0) {
+	// 		convertedHours.push(convertHours);
+	// 	}
+	// } while(Math.floor(convertHours) > 0)
+
+
+	// do {
+	// 	if(convertMinutes > 0) {
+
+	// 		// ConvertedHours.push(Math.floor(convertHours % 2))
+	// 		convertedMinutes.push(Math.floor(convertMinutes % 2));
+			
+	// 		// console.log(convertMinutes % 2);
+
+	// 		// convertHours = convertHours / 2;
+	// 		convertMinutes = convertMinutes / 2;
+
+	// 		// console.log(convertMinutes);
+			
+			
+	// 	} else if (Math.floor(convertMinutes) === 1 || Math.floor(convertMinutes) === 0) {
+	// 		// console.log(`I'm in ${convert}`);
+			
+	// 		convertedMinutes.push(convertMinutes);
+
+	// 		// console.log(convertedMinutes.length);
+			
+	// 	}
+
+	// 	// console.log(`ConvertedMinutes: ${convertedMinutes}`);
+		
+
+	
+		
+	// }while(Math.floor(convertMinutes) > 0)
+
+	// do {
+
+	// 	if(convertSeconds > 0){
+	// 		convertedSeconds.push(Math.floor(convertSeconds % 2));
+
+	// 		convertSeconds = convertSeconds / 2;
+	// 	} else if (Math.floor(convertSeconds) === 1 || Math.floor(convertSeconds) === 0) {
+	// 		convertedSeconds.push(convertSeconds);
+	// 	}
+	// } while(Math.floor(convertSeconds) > 0)
+
+	// if (convertedHours.length < 6) {
+	// 	const addZero = 6 - convertedHours.length;
+
+	// 	for(let i = 0; i < addZero; i ++) {
+	// 		convertedHours.push(0);
+	// 	}
+	// }
+
 	convertedHours.map(x => binaryMarkup(x, hoursClass));
+
+	// console.log(convertedMinutes.length);
+	// if (convertedMinutes.length < 6) {
+	// 	const addZero = 6 - convertedMinutes.length;
+
+	// 	for(let i = 0; i < addZero; i ++) {
+	// 		convertedMinutes.push(0);
+	// 	}
+	// }
 	convertedMinutes.map(x => binaryMarkup(x, minutesClass));
+
+	// if (convertedSeconds.length < 6) {
+	// 	const addZero = 6 - convertedSeconds.length;
+
+	// 	for(let i = 0; i < addZero; i ++) {
+	// 		convertedSeconds.push(0);
+	// 	}
+	// }
 	convertedSeconds.map(x => binaryMarkup(x, secondsClass));
+
+	
+	
 }
 
 const convertTimeToBinary = (time) => {
@@ -115,6 +210,9 @@ const convertTimeToBinary = (time) => {
 }
 
 const binaryMarkup = (number, e) => {
+
+	// TODO Entra aqui um novo argumento com a class onde o markup vai inserir
+
 	// Perform the binary markup
 	if(number === 1) {
 		const markup = `<div class="container__binary-watch--full-color">&nbsp;</div>`;
